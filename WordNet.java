89177407c4652441b68af3d3c062006be1b036a5
Harry <oyjunhan@hotmail.com>
2025-02-28 09:43:34 -0800

    Fetch API for project files

---
+++ WordNet.java
@@ -0,0 +1,26 @@
+/**
+ * Created on Fri Feb 28 2025
+ * @author Harry Ouyang attests that this code is their original work and was written in compliance with the class Academic Integrity and Collaboration Policy found in the syllabus.
+ */
+
+public class WordNet {
+
+   // constructor takes the name of the two input files
+   public WordNet(String synsets, String hypernyms)
+
+   // returns all WordNet nouns
+   public Iterable<String> nouns()
+
+   // is the word a WordNet noun?
+   public boolean isNoun(String word)
+
+   // distance between nounA and nounB (defined below)
+   public int distance(String nounA, String nounB)
+
+   // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+   // in a shortest ancestral path (defined below)
+   public String sap(String nounA, String nounB)
+
+   // do unit testing of this class
+   public static void main(String[] args)
+}
